cf54451b778b8daf241a932ff228748f3b16eab1
tzuleger <traviszuleger@yahoo.com>
2023-08-01 14:27:34 -0500

    initialize

---
+++ tsup.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'tsup';
+export default defineConfig({
+    entryPoints: [
+        'src/index.js',
+        'src/exceptions.js',
+        'src/utils.js',
+    ],
+    entry: {
+        index: "src/index.js",
+        errors: "src/exceptions.js",
+        util: "src/util.js",
+    },
+    outDir: 'dist',
+    target: 'node22',
+    format: ['esm'],
+    treeshake: true,
+    clean: true,
+    minify: true,
+});
